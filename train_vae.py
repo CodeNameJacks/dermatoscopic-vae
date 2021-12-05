79e852b4d94b182b959d4a50feb3ae8cf06c2b9d
Ritesh Ahlawat <riteshahlawat1@gmail.com>
2021-12-04 23:04:17 -0500

    Updated code, will test on colab

--- train_vae.py
+++ train_vae.py
@@ -2,10 +2,10 @@ import argparse
 import multiprocessing
 from operator import itemgetter
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('speech_commands_dir', type=str, help='Path to speech commands dataset\'s category ex: `speech_commands/cat`.')
+    parser.add_argument('speech_commands_dir', type=str,
+                        help='Path to speech commands dataset\'s category ex: `speech_commands/cat`.')
     parser.add_argument('-bs', '--batch_size', type=int, default=32, help='Batch size for the model (default - 32).')
     parser.add_argument('-nw', '--num_workers', type=int, default=multiprocessing.cpu_count(),
                         help='Maximum number of processes to spin up when using process-based threading (default - number of cores [multiprocessing.cpu_count()]).')
@@ -22,8 +22,9 @@ import tensorflow as tf
 import tensorflow.keras as keras
 import numpy as np
 import os
-from os import path
 from sklearn.model_selection import train_test_split
+from clr_callback import CyclicLR
+from librosa.display import specshow
 
 seed = 42
 
@@ -48,12 +49,14 @@ class ConvolutionalVAE(keras.Model):
         self.code_dim_size = code_dim_size
         self.target_image_dims = target_image_dims
 
-        encoder_inputs = keras.Input(shape=target_image_dims)  # (124, 128, 1)
+        encoder_inputs = keras.Input(shape=target_image_dims)  # (136, 64, 1)
         num_conv_layers = 2
         kernels = 3
         strides = 2
-        x = keras.layers.Conv2D(filters=16, kernel_size=kernels, strides=strides, padding='same', activation='relu')(encoder_inputs)  # (62, 64, 32)
-        x = keras.layers.Conv2D(filters=32, kernel_size=kernels, strides=strides, padding='same', activation='relu')(x)  # (31, 32, 64)
+        x = keras.layers.Conv2D(filters=32, kernel_size=kernels, strides=strides, padding='same', activation='relu')(encoder_inputs)
+        x = keras.layers.Conv2D(filters=64, kernel_size=kernels, strides=strides, padding='same', activation='relu')(x)
+        # x = keras.layers.Conv2D(filters=128, kernel_size=kernels, strides=strides, padding='same', activation='relu')(
+        #     x)  # (16, 16, 128)
         x = keras.layers.Flatten()(x)
         x = keras.layers.Dense(16, activation='relu')(x)
         z_mean = keras.layers.Dense(code_dim_size, name='z_mean')(x)
@@ -62,75 +65,92 @@ class ConvolutionalVAE(keras.Model):
         self.encoder = keras.Model(encoder_inputs, [z_mean, z_logvar, z], name='encoder')
         self.encoder.summary()
 
+        decode_image_shape_x = int(target_image_dims[0] / (2 ** num_conv_layers))
+        decode_image_shape_y = int(target_image_dims[1] / (2 ** num_conv_layers))
         latent_input = keras.Input(shape=(code_dim_size,))
-        decode_image_x_shape = int(target_image_dims[0] / (num_conv_layers * strides))
-        decode_image_y_shape = int(target_image_dims[1] / (num_conv_layers * strides))
-
-        x = keras.layers.Dense(decode_image_x_shape * decode_image_y_shape * 32, activation='relu')(latent_input)
-        x = keras.layers.Reshape((decode_image_x_shape, decode_image_y_shape, 32))(x)
-        x = keras.layers.Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(x)
-        x = keras.layers.Conv2DTranspose(filters=16, kernel_size=3, strides=2, padding='same', activation='relu')(x)
-        decoder_output = keras.layers.Conv2DTranspose(filters=1, kernel_size=3, strides=1, padding='same')(x)
+        d = keras.layers.Dense(decode_image_shape_x * decode_image_shape_y * 64, activation='relu')(latent_input)
+        d = keras.layers.Reshape((decode_image_shape_x, decode_image_shape_y, 64))(d)
+        # x = keras.layers.Conv2DTranspose(filters=128, kernel_size=3, strides=2, padding='same', activation='relu')(x)
+        d = keras.layers.Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', activation='relu')(d)
+        d = keras.layers.Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(d)
+        decoder_output = keras.layers.Conv2DTranspose(filters=1, kernel_size=3, strides=1, padding='same')(d)
         decoder_output = keras.layers.Activation('sigmoid', dtype='float32')(decoder_output)
         self.decoder = keras.Model(latent_input, decoder_output, name='decoder')
         self.decoder.summary()
 
         self.total_loss_tracker = keras.metrics.Mean(name='total_loss')
-        self.reconstruction_loss_tracker = keras.metrics.Mean(name='reconstruction_loss')
-        self.kl_loss_tracker = keras.metrics.Mean(name='kl_loss')
+        # self.reconstruction_loss_tracker = keras.metrics.Mean(name='reconstruction_loss')
+        # self.kl_loss_tracker = keras.metrics.Mean(name='kl_loss')
         self.mse_tracker = keras.metrics.Mean(name='mse')
 
     @property
     def metrics(self):
         return [
             self.total_loss_tracker,
-            self.reconstruction_loss_tracker,
-            self.kl_loss_tracker,
+            # self.reconstruction_loss_tracker,
+            # self.kl_loss_tracker,
             self.mse_tracker
         ]
+
+    def log_normal_pdf(self, sample, mean, logvar, raxis=1):
+        log2pi = tf.math.log(2. * np.pi)
+        return tf.reduce_sum(
+            -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+            axis=raxis)
 
     def call(self, inputs, training=None, mask=None):
         z_mean, z_logvar, z = self.encoder(inputs)
         reconstruction = self.decoder(z)
-        reconstruction_loss = tf.reduce_mean(
-            tf.reduce_sum(keras.losses.binary_crossentropy(inputs, reconstruction)))
-        kl_loss = -0.5 * (1 + z_logvar - tf.square(z_mean) - tf.exp(z_logvar))
-        kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss))
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=reconstruction, labels=inputs)
+        logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+        logpz = self.log_normal_pdf(z, 0., 0.)
+        logqz_x = self.log_normal_pdf(z, z_mean, z_logvar)
+        total = -tf.reduce_mean(logpx_z + logpz - logqz_x)
+        # reconstruction_loss = tf.reduce_mean(
+        #     tf.reduce_sum(keras.losses.binary_crossentropy(inputs, reconstruction)))
+        # kl_loss = -0.5 * (1 + z_logvar - tf.square(z_mean) - tf.exp(z_logvar))
+        # kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss))
         # Casting only required when using mixed_precision
         # Without it, it results in a float16 and float32 type mismatch
-        total_loss = reconstruction_loss + tf.cast(kl_loss, dtype=reconstruction_loss.dtype)
+        # total_loss = reconstruction_loss + tf.cast(kl_loss, dtype=reconstruction_loss.dtype)
         mse_loss = tf.reduce_mean(keras.losses.mean_squared_error(inputs, reconstruction))
 
-        self.add_metric(kl_loss, name='kl_loss', aggregation='mean')
-        self.add_metric(total_loss, name='total_loss', aggregation='mean')
-        self.add_metric(reconstruction_loss, name='reconstruction_loss', aggregation='mean')
+        # self.add_metric(kl_loss, name='kl_loss', aggregation='mean')
+        self.add_metric(total, name='total_loss', aggregation='mean')
+        # self.add_metric(reconstruction_loss, name='reconstruction_loss', aggregation='mean')
         self.add_metric(mse_loss, name='mse', aggregation='mean')
         return reconstruction
 
     def train_step(self, data):
         with tf.GradientTape() as tape:
+            # plot_spectrogram(data[0], batched=True)
             z_mean, z_logvar, z = self.encoder(data[0])
             reconstruction = self.decoder(z)
-            reconstruction_loss = tf.reduce_mean(
-                tf.reduce_sum(keras.losses.binary_crossentropy(data[0], reconstruction), axis=(1, 2)))
-            kl_loss = -0.5 * (1 + z_logvar - tf.square(z_mean) - tf.exp(z_logvar))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=reconstruction, labels=data[0])
+            logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+            logpz = self.log_normal_pdf(z, 0., 0.)
+            logqz_x = self.log_normal_pdf(z, z_mean, z_logvar)
+            total = -tf.reduce_mean(logpx_z + logpz - logqz_x)
+            # reconstruction_loss = tf.reduce_mean(
+            #     tf.reduce_sum(keras.losses.binary_crossentropy(data[0], reconstruction), axis=(1, 2)))
+            # kl_loss = -0.5 * (1 + z_logvar - tf.square(z_mean) - tf.exp(z_logvar))
+            # kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             # Casting only required when using mixed_precision
             # Without it, it results in a float16 and float32 type mismatch
-            total_loss = reconstruction_loss + tf.cast(kl_loss, dtype=reconstruction_loss.dtype)
-            mse_loss = tf.reduce_mean(keras.losses.mean_squared_error(data[0], reconstruction), axis=(1, 2))
+            # total_loss = reconstruction_loss + tf.cast(kl_loss, dtype=reconstruction_loss.dtype)
+            mse_loss = tf.reduce_mean(keras.losses.mean_squared_error(data[0], reconstruction))
 
-        gradient = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(gradient, self.trainable_weights))
-        self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
+        gradient = tape.gradient(total, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradient, self.trainable_variables))
+        self.total_loss_tracker.update_state(total)
+        # self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        # self.kl_loss_tracker.update_state(kl_loss)
         self.mse_tracker.update_state(mse_loss)
 
         return {
             "loss": self.total_loss_tracker.result(),
-            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result(),
+            # "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            # "kl_loss": self.kl_loss_tracker.result(),
             "mse": self.mse_tracker.result(),
         }
 
@@ -149,8 +169,8 @@ def initialize_training_env():
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             print(e)
-    policy = keras.mixed_precision.Policy('mixed_float16')
-    keras.mixed_precision.set_global_policy(policy)
+    # policy = keras.mixed_precision.Policy('mixed_float16')
+    # keras.mixed_precision.set_global_policy(policy)
 
 
 def decode_audio(audio_binary):
@@ -170,7 +190,23 @@ def get_waveform_and_label(file_path):
     return spectrogram
 
 
+def _tf_log10(x):
+    numerator = tf.math.log(x)
+    denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
+    return numerator / denominator
+
+
+def power_to_db(magnitude, amin=1e-16, top_db=80.0):
+    ref_value = tf.reduce_max(magnitude)
+    log_spec = 10.0 * _tf_log10(tf.maximum(amin, magnitude))
+    log_spec -= 10.0 * _tf_log10(tf.maximum(amin, ref_value))
+    log_spec = tf.maximum(log_spec, tf.reduce_max(log_spec) - top_db)
+
+    return log_spec
+
+
 def get_spectrogram(waveform):
+    # pydevd.settrace(suspend=False)
     # zero-padding for an audio waveform with less than 16,000 samples
     input_len = 16000
     waveform = waveform[:input_len]
@@ -181,37 +217,91 @@ def get_spectrogram(waveform):
     # audio clips are of the same length.decode_audio
     equal_length = tf.concat([waveform, zero_padding], axis=0)
     # convert the waveform to a spectrogram via a STFT.
-    spectrogram = tf.signal.stft(equal_length, frame_length=255, frame_step=128)
-    # Reduce frames by 1 if it's odd (convolutional needs even inputs or else it gets messed up)
-    if spectrogram.shape[-1] % 2 == 1:
-        spectrogram = spectrogram[:, :-1]
-    # obtain the magnitude of the STFT.
+    fft_size = 256
+    hop_size = 116
+    sample_rate = input_len
+    spectrogram = tf.signal.stft(equal_length, frame_length=fft_size, frame_step=hop_size, fft_length=fft_size)
     spectrogram = tf.abs(spectrogram)
+    num_spectrogram_bins = (fft_size // 2) + 1
+    lower_edge_hertz, upper_edge_hertz, num_mel_bins = 80.0, sample_rate / 2, 64
+    mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(num_mel_bins, num_spectrogram_bins, sample_rate,
+                                                              lower_edge_hertz, upper_edge_hertz)
+    mel_spectrogram = tf.matmul(tf.square(spectrogram), mel_weight_matrix)
+    log_mel_spectrogram = tf.transpose(tf.math.log(mel_spectrogram + 1e-6))
 
-    # add a channels dimension, so that the spectrogram can be used
-    # as image-like input data with conv layers (which expect
-    # shape (batch_size, height, width, channels)).
-    spectrogram = spectrogram[..., tf.newaxis]
-    return spectrogram
+    return log_mel_spectrogram[..., tf.newaxis]
+
+
+def plot_single_spectrogram(spec):
+    spec = np.squeeze(spec)
+    plt.clf()
+    plt.figure()
+    specshow(spec, y_axis='mel', x_axis='time', sr=16000, hop_length=116)
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Log-magnitude mel-scaled spectrogram')
+    plt.show()
+    plt.close()
+
+
+def plot_spectrogram(spectrograms, i=None, batched=False):
+    if i is not None:
+        os.makedirs('plots', exist_ok=True)
+    plt.clf()
+    plt.figure()
+    if batched:
+        spectrograms = np.squeeze(spectrograms)
+        for spec in spectrograms:
+            plt.clf()
+            plt.figure()
+            specshow(spec, y_axis='mel', x_axis='time', sr=16000, hop_length=116)
+            plt.colorbar(format='%+2.0f dB')
+            plt.title('Log-magnitude mel-scaled spectrogram')
+            plt.show()
+            plt.close()
+    else:
+        spectrograms = np.squeeze(spectrograms)
+        specshow(spectrograms, y_axis='mel', x_axis='time', sr=16000, hop_length=116)
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Log-magnitude mel-scaled spectrogram')
+        if i is not None:
+            plt.savefig(f'plots/{i}.jpeg', dpi=300)
+        else:
+            plt.show()
+        plt.close()
 
 
 def preprocess_dataset(files):
     files_ds = tf.data.Dataset.from_tensor_slices(files)
     # output_ds = files_ds.map(map_func=get_waveform_and_label, num_parallel_calls=tf.data.AUTOTUNE)
-    output_ds = files_ds.map(lambda x: tf.py_function(get_waveform_and_label, [x], [tf.float32]), num_parallel_calls=tf.data.AUTOTUNE)
+    output_ds = files_ds.map(lambda x: tf.py_function(get_waveform_and_label, [x], [tf.float32]),
+                             num_parallel_calls=tf.data.AUTOTUNE)
     return output_ds
 
 
-def generate_and_save_images(model: ConvolutionalVAE, epoch, test_sample):
-    mean, logvar = model.encode(test_sample)
-    z = model.reparameterize(mean, logvar)
-    predictions = model.sample(z)
-    fig = plt.figure(figsize=(4, 4))
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i + i)
-        plt.imshow(predictions[i, :, :, 0], cmap='gray')
-        plt.axis('off')
-    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+def plot_history_metric(metrics, history: keras.callbacks.History, clr=None, savefiles=None):
+    for metric in metrics:
+        val = f'val_{metric}'
+        plt.clf()
+        plt.figure()
+        plt.plot(history.history[metric])
+        if val in history.history.keys():
+            plt.plot(history.history[val])
+        plt.title(f'{metric.capitalize()}')
+        plt.ylabel(f'{metric.capitalize()}')
+        plt.xlabel('Epoch')
+        if val in history.history.keys():
+            plt.legend(['train', 'val'], loc='upper left')
+        if not savefiles:
+            plt.show()
+    if clr is not None:
+        plt.clf()
+        plt.figure()
+        plt.plot(clr.history['lr'])
+        plt.title('Learning Rate')
+        plt.ylabel('LR')
+        plt.xlabel('Iteration')
+        if not savefiles:
+            plt.show()
 
 
 def main(args):
@@ -228,7 +318,7 @@ def main(args):
 
     checkpoint_path = 'training/cp{epoch:02d}-{val_loss:.2f}.ckpt'
     # Get speech commands
-    code_dim_size = 2
+    code_dim_size = 16
     # Get file paths of audio files
     filenames = tf.io.gfile.glob(speech_commands_dir + '/*')
     filenames = tf.random.shuffle(filenames).numpy()
@@ -253,11 +343,15 @@ def main(args):
     train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
     val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
 
-    for spec, in test_ds.take(1):
-        input_shape = spec.shape
-    print('Input shape:', input_shape)
+    for i, spec, in enumerate(test_ds.take(1)):
+        input_shape = spec[0].shape
+        # plot_spectrogram(spec[0].numpy(), i)
+
+        print('Input shape:', input_shape)
 
     strategy = tf.distribute.MirroredStrategy()
+    clr = CyclicLR(base_lr=0.0005, max_lr=0.01, step_size=500., mode='triangular2')
+
     print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
     if strategy.num_replicas_in_sync > 1:
         with strategy.scope():
@@ -265,8 +359,15 @@ def main(args):
             autoencoder.compile(optimizer=keras.optimizers.Adam())
     else:
         autoencoder = ConvolutionalVAE(code_dim_size=code_dim_size, target_image_dims=input_shape)
-        autoencoder.compile(optimizer=keras.optimizers.Adam(clipnorm=1.0))
-    autoencoder.fit(train_ds, validation_data=val_ds, epochs=30, workers=num_workers, max_queue_size=30)
+        autoencoder.compile(optimizer=keras.optimizers.Adam(), run_eagerly=False)
+    history = autoencoder.fit(train_ds, validation_data=val_ds, epochs=60, workers=num_workers,
+                              max_queue_size=30, callbacks=[clr])
+    # plot_history_metric(['loss'], history, clr=clr)
+    for test_spec, in test_ds.take(1):
+        plot_single_spectrogram(test_spec.numpy())
+        _, _, z = autoencoder.encoder(test_spec[tf.newaxis, ...])
+        reconstruction_spec = autoencoder.decoder(z)
+        plot_single_spectrogram(reconstruction_spec)
 
 
 if __name__ == "__main__":
